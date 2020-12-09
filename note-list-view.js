498f0c2268e0550578b575a9da15e3d9ccc2d6e9
savannaelbey <savanna_e@icloud.com>
2020-12-09 00:30:46 +0000

    created index.html file with minimal HTML, installed node and http-server from npm

--- note-list-view.js
+++ note-list-view.js
@@ -13,7 +13,7 @@ class NoteListView {
     let before = '<ul><li><div>';
     let after = '</div></li></ul>';
     let middle = this.noteList.list.join('</div></li><li><div>');
-    return before + middle + after;
+    return `${before}${middle}${after}`;
   }
 
 }
