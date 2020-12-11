daf1820d76d9643c7882bcde74efe8ac1c7bb876
savannaelbey <savanna_e@icloud.com>
2020-12-11 15:14:06 +0000

    mocked note controller tests

--- tests/note-controller-tests.js
+++ tests/note-controller-tests.js
@@ -1,12 +1,26 @@
 'use strict';
 it('can be instantiated', function() {
-  let noteList;
-  let noteController = new NoteController(noteList);
+  class NoteListDouble {
+    addNote() {
+
+    }
+  }
+  let noteListDouble = new NoteListDouble();
+  let noteController = new NoteController(noteListDouble);
   expect(noteController instanceof NoteController).toEqual(true);
 });
 
 it('updates the innerHTML of the element with the app tag', function() {
-  let noteController = new NoteController();
+  class NoteListDouble {
+    constructor() {
+      this.list = ['Favourite drink: Apple juice']
+    }
+    addNote(text) {
+      return ['Favourite drink: Apple juice'];
+    }
+  }
+  let noteListDouble = new NoteListDouble();
+  let noteController = new NoteController(noteListDouble);
   noteController.display();
   expect(document.getElementById('app').innerHTML).toEqual('<ul><li><div>Favourite drink: Apple juice</div></li></ul>')
 });
